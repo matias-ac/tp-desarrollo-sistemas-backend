c381f0ea332d43ce471a9ebba59d8d96445ad48c
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-18 16:20:34 -0300

    agrega las relaciones a Usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,8 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class Usuario(AbstractUser):
+    supermercados_favoritos = models.ManyToManyField(
+        "entidades.Supermercado", related_name="usuarios_favoritos", blank=True
+    )
+    promociones_favoritas = models.ManyToManyField(
+        "promociones.Promocion", related_name="usuarios_favoritos", blank=True
+    )
 
     def __str__(self):
         nombre_completo = f"{self.first_name} {self.last_name}".strip()
