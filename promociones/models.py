d4ca286a584cf4b7e8f377d7b14b495cdeaee23f
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-18 15:28:14 -0300

    fix: actualiza campos opcionales segun DER y modifica retornos de los metodos __str__

--- promociones/models.py
+++ promociones/models.py
@@ -5,25 +5,29 @@ class MedioDePago(models.Model):
     descripcion = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descripcion
+        return f"{self.descripcion}"
 
 
 class DiaSemana(models.Model):
     nombre = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}"
 
 
 class Promocion(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
-    porcentajeDescuento = models.DecimalField(max_digits=5, decimal_places=2)
-    topeReintegro = models.DecimalField(max_digits=10, decimal_places=2)
-    cuotasDisponibles = models.IntegerField()
-    fechaInicio = models.DateField()
-    fechaFin = models.DateField()
-    condicionesLegales = models.TextField()
+    porcentaje_descuento = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
+    tope_reintegro = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
+    cuotas_disponibles = models.IntegerField(null=True, blank=True)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    condiciones_legales = models.TextField()
 
     def __str__(self):
-        return self.titulo
+        return f"{self.titulo}"
