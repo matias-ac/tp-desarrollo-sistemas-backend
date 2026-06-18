298b04a959668fc59a3943eaebc342c2a2600b15
lucasabrandan <84356343+lucasabrandan@users.noreply.github.com>
2026-06-18 14:50:50 -0300

    feat: crear modelos MedioDePago y DiaSemana (#6)
    Se crean los modelos MedioDePago y DiaSemana en promociones/models.py con sus respectivos atributos y método str().

--- promociones/models.py
+++ promociones/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MedioDePago(models.Model):
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descripcion
+
+
+class DiaSemana(models.Model):
+    nombre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
