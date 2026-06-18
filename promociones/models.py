6861fef473d07143047bd9d9f29db4279f69a263
lucasabrandan <84356343+lucasabrandan@users.noreply.github.com>
2026-06-18 14:58:16 -0300

    feat: crear modelo Promocion con atributos core (#7)
    * feat: crear modelos MedioDePago y DiaSemana
    
    * feat: crear modelo Promocion con atributos core
    
    ---------
    
    Co-authored-by: Matias Ariel Castro <matias.acastro1@gmail.com>

--- promociones/models.py
+++ promociones/models.py
@@ -13,3 +13,17 @@ class DiaSemana(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Promocion(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    porcentajeDescuento = models.DecimalField(max_digits=5, decimal_places=2)
+    topeReintegro = models.DecimalField(max_digits=10, decimal_places=2)
+    cuotasDisponibles = models.IntegerField()
+    fechaInicio = models.DateField()
+    fechaFin = models.DateField()
+    condicionesLegales = models.TextField()
+
+    def __str__(self):
+        return self.titulo
